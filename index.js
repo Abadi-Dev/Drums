d554b4b2546618f887ec30b53dc722b45f955041
Abadi-Dev <65283737+Abadi-Dev@users.noreply.github.com>
2022-03-09 23:42:25 +0300

    added event listeners
    added event, still need to work on audio files.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
     for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
         document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-            var audio = new Audio("sounds/tom-1.mp3");
-            audio.play();
-            this.style.color = "blue";
+            // var audio = new Audio("sounds/tom-1.mp3");
+            // audio.play();
+            //this.style.color = "blue";
         });
 
     }
